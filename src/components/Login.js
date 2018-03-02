23a085423fc561081c523f2016c8219027d4341e
Diego Gil <diego.gil2505@gmail.com>
2018-03-02 16:16:00 -0300

    Added form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,48 +1,95 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 import { Button, Form, Grid, Header, Segment } from 'semantic-ui-react';
-import {Link} from "react-router-dom";
 
-// I got this from semantic-ui sample pages
-const LoginForm = () => (
-  <div className='login-form'>
-    <style>{`
-      body > div,
-      body > div > div,
-      body > div > div > div.login-form {
-        height: 100%;
-      }
-    `}</style>
-    <Grid
-      textAlign='center'
-      style={{ height: '100%' }}
-      verticalAlign='middle'
-    >
-      <Grid.Column style={{ maxWidth: 450 }}>
-        <Header as='h2' color='orange' textAlign='center'>
-          Deliver Games Private Area
-        </Header>
-        <Form size='large'>
-          <Segment stacked>
-            <Form.Input
-              fluid
-              icon='user'
-              iconPosition='left'
-              placeholder='E-mail address'
-            />
-            <Form.Input
-              fluid
-              icon='lock'
-              iconPosition='left'
-              placeholder='Password'
-              type='password'
-            />
+class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      submitClicked: false,
+    };
+  }
 
-            <Button as={Link} to='/admin' color='orange' fluid size='large'>Login</Button>
-          </Segment>
-        </Form>
-      </Grid.Column>
-    </Grid>
-  </div>
-)
+  renderField(field) {
+    return (
+      <Form.Input
+        fluid
+        iconPosition='left'
+        icon={field.icon}
+        placeholder={field.placeholder}
+        type={field.type}
+        error={field.meta.touched && field.meta.error != null }
+        {...field.input}
+      />
+    );
+  }
 
-export default LoginForm
+  onSubmit(values) {
+    this.setState({ submitClicked: true });
+    console.warn(this.props);
+  }
+
+  render() {
+    const { handleSubmit, submitted } = this.props;
+
+    return (
+      <div className='login-form'>
+        <style>{`
+        body > div,
+        body > div > div,
+        body > div > div > div.login-form {
+          height: 100%;
+        }
+      `}</style>
+        <Grid
+          textAlign='center'
+          style={{height: '100%'}}
+          verticalAlign='middle'
+        >
+          <Grid.Column style={{maxWidth: 450}}>
+            <Header as='h2' color='orange' textAlign='center'>
+              Deliver Games Private Area
+            </Header>
+            <Form as="form" size='large' onSubmit={ handleSubmit(this.onSubmit.bind(this)) }>
+              <Segment stacked>
+                <Field
+                  name="user"
+                  icon="user"
+                  placeholder="E-mail or username"
+                  type="text"
+                  component={this.renderField}
+                />
+                <Field
+                  name="password"
+                  icon="lock"
+                  placeholder="Password"
+                  type="password"
+                  component={this.renderField}
+                />
+                <Button disabled={submitted} color='orange' fluid size='large'>Login</Button>
+              </Segment>
+            </Form>
+          </Grid.Column>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+function validate(values) {
+  const errors = {};
+
+  if (!values.user) {
+    errors.user = "This username/e-mail does not please me..";
+  }
+  if (!values.password) {
+    errors.password = "You need a password to login bro";
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'LoginForm',
+})(LoginForm);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as reduxFormReducer } from 'redux-form';
+
 import UsersReducer from './UsersReducer';
 import ActiveUserReducer from './ActiveUserReducer';
 
 const rootReducer = combineReducers({
+  form: reduxFormReducer,
   users: UsersReducer,
   activeUser: ActiveUserReducer,
 });
